444f2f088e664e4d2a8bf5b880e326141b122f9b
gugugaga-png <lazwardi825@gmail.com>
2025-05-15 19:00:20 +0700

    asdasdasdaaa

--- database/migrations/2025_05_13_015741_add_role_id_to_users_table.php
+++ database/migrations/2025_05_13_015741_add_role_id_to_users_table.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,16 +10,11 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            // Tambahkan default(2) jika ingin role_id otomatis = 2
-            $table->foreignId('role_id')
-                  ->nullable()
-                  ->default(2)
-                  ->constrained()
-                  ->onDelete('set null');
-        });
-    }
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->foreignId('role_id')->nullable()->constrained()->onDelete('set null');
+    });
+}
 
     /**
      * Reverse the migrations.
@@ -28,9 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            // Harus drop foreign key & kolom saat rollback
-            $table->dropForeign(['role_id']);
-            $table->dropColumn('role_id');
+            //
         });
     }
 };
